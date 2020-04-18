776f3e1e4131077eba5f5c1a53f2fe4fbf3218bb
Fedorova0410 <50097427+Fedorova0410@users.noreply.github.com>
2020-04-18 19:04:26 +0300

    3 day, 1:19:40

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movie');
-const urlPoster = 'https://image.tmdb.org/t/p/w500/';
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(event) {
     event.preventDefault();
 
     const searchText = document.querySelector('.form-control').value;
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=9bbf2aadf327c17b26d2c35440604b5d&language=ru&query=' + searchText;
-    movie.innerHTML = 'Загрузка...';
 
-    fetch(server)
+    if (searchText.length === 0) {
+        movie.trim().innerHTML = '<h2 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h2>'
+        return
+    }
+
+    movie.innerHTML = '<div class="spinner"></div>';
+
+    fetch('https://api.themoviedb.org/3/search/multi?api_key=9bbf2aadf327c17b26d2c35440604b5d&language=ru&query=' + searchText)
         .then(function(value) {
 
             if (value.status !== 200) {
@@ -19,23 +24,89 @@ function apiSearch(event) {
 
         })
         .then(function(output) {
-
             let inner = '';
+
+            if (output.results.length === 0) {
+                inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>';
+
+            };
+
             output.results.forEach(function(item) {
                 let nameItem = item.name || item.title;
+                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noPoster.png';
+                let dataInfo = '';
+
+                if (item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+
                 inner += `
-                <div class="col-12 col-md-4 col-xl-3 item">
-                <img src="${urlPoster + item.poster_path}" alt="${nameItem}">
+                <div class="col-12 col-md-6 col-xl-3 item">
+                <img src="${poster}" alt="${nameItem}" ${dataInfo}>
                 <h5>${nameItem}</h5></div>
-                `
+                `;
             });
             movie.innerHTML = inner;
+
+            addEventMedia();
+
         })
         .catch(function(err) {
             movie.innerHTML = 'Упс, что-то пошло не так!';
             console.error('error ' + err)
         })
-
 };
 
 searchForm.addEventListener('submit', apiSearch);
+
+function addEventMedia() {
+    const media = movie.querySelectorAll('img[data-id]');
+    media.forEach(function(elem) {
+        elem.style.cursor = 'pointer';
+        elem.addEventListener('click', showFullInfo);
+    })
+}
+
+function showFullInfo() {
+    console.log(this)
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=9bbf2aadf327c17b26d2c35440604b5d&language=ru')
+        .then(function(value) {
+
+            if (value.status !== 200) {
+                return Promise.reject(value);
+            }
+            return value.json()
+
+        })
+        .then(function(output) {
+            let inner = '<h2 class="col-12 text-center text-info">Популярные за неделю</h2>';
+
+            if (output.results.length === 0) {
+                inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>';
+
+            };
+
+            output.results.forEach(function(item) {
+                let nameItem = item.name || item.title;
+                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noPoster.png';
+                let dataInfo = '';
+
+                if (item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+
+                inner += `
+            <div class="col-12 col-md-6 col-xl-3 item">
+            <img src="${poster}" alt="${nameItem}" ${dataInfo}>
+            <h5>${nameItem}</h5></div>
+            `;
+            });
+            movie.innerHTML = inner;
+
+            addEventMedia();
+
+        })
+        .catch(function(err) {
+            movie.innerHTML = 'Упс, что-то пошло не так!';
+            console.error('error ' + err)
+        })
+})
